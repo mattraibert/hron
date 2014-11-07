af202bbe82a3cf8519ea5b8d93245912474b28c2
Matt Raibert <mattraibert@gmail.com>
2014-11-06 21:51:45 -0500

    run arbitrary process

--- Main.hs
+++ Main.hs
@@ -3,15 +3,15 @@ import Control.Monad (forever)
 import System.Process
 
 main = do
-  _ <- forkIO $ g 1 "frequent process"
-  _ <- forkIO $ g 3 "less frequent..."
-  _ <- forkIO $ g 10 "big slow process.."
+  _ <- forkIO $ f 1 $ echo "frequent process"
+  _ <- forkIO $ f 3 $ echo "less frequent..."
+  _ <- forkIO $ f 10 $ ls
   forever $ threadDelay 1000
     where
-      g t msg = mapM_ (f t msg) (repeat 1)
-      f :: Int -> String -> Int -> IO ()
-      f t msg _ = do
+      ls = runProcess "ls" [] Nothing Nothing Nothing Nothing Nothing
+      echo msg = runProcess "echo" [msg] Nothing Nothing Nothing Nothing Nothing
+      f :: Int -> (IO a) -> IO ()
+      f t msg = forever $ do
             threadDelay $ t * 1000 * 1000
-            -- _ <- runProcess "echo" [msg] Nothing Nothing Nothing Nothing Nothing
-            print msg
+            _ <- msg -- 
             return ()
